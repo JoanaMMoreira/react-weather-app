d7b2f973c7c5dde893b25ed519c54182871e9b20
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2019-01-13 17:14:44 +0000

    Updates

---
+++ precache-manifest.0930112d8c32b33d9d53c852281b4719.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "a193d3b309692ee827e7",
+    "url": "/joanammoreira.github.io/react-weather-app/static/css/main.fef90be0.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "069850f6983dc6933c08",
+    "url": "/joanammoreira.github.io/react-weather-app/static/js/1.069850f6.chunk.js"
+  },
+  {
+    "revision": "8154d976005e35d92cf9",
+    "url": "/joanammoreira.github.io/react-weather-app/static/js/runtime~main.8154d976.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "a193d3b309692ee827e7",
+    "url": "/joanammoreira.github.io/react-weather-app/static/js/main.a193d3b3.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/joanammoreira.github.io/react-weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "069850f6983dc6933c08",
+    "url": "/joanammoreira.github.io/react-weather-app/static/css/1.c50de872.chunk.css"
+  },
+  {
+    "revision": "5ab79a719533cdcac055bfdd0064fde1",
+    "url": "/joanammoreira.github.io/react-weather-app/index.html"
+  }
+];
